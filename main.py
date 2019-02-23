1fce725b0717a745cd2f644049731da8a979813f
Luke <elgrimm666@gmail.com>
2019-02-23 21:40:27 +0100

    Python

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import RPi.GPIO as GPIO
-
+import time
 from pi_sht1x import SHT1x
+from os import system
 
+# Gelb = Clock
+# Blau = Data
+# Rot = Strom
+# Green = Ground
+
+DATA_PIN = 24;
+SCK_PIN = 23;
 
 def main():
-     print("start");
-     with SHT1x(18, 23, gpio_mode=GPIO.BCM) as sensor:
-         temp = sensor.read_temperature()
-         humidity = sensor.read_humidity(temp)
-         sensor.calculate_dew_point(temp, humidity)
-         print(sensor)
+     with SHT1x(DATA_PIN, SCK_PIN, gpio_mode=GPIO.BCM) as sensor:
+          while 1:
+               system("clear");
+               temp = sensor.read_temperature()
+               humidity = sensor.read_humidity(temp)
+               sensor.calculate_dew_point(temp, humidity)
+               print(sensor)
+               time.sleep(2)
+               
+          
 
 
 main();
