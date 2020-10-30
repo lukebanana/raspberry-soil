5b8798fd337ca08d73f24defc7230e54d87e55f6
Luke <elgrimm666@gmail.com>
2020-10-30 23:39:51 +0100

    New setup + CSV

--- app.js
+++ app.js
@@ -2,52 +2,35 @@ var msg = 'Starting...';
 console.log(msg);
 
 fs = require('fs');
-var async = require('async');
-var sht10 = require('./js/SHT1x')
 
 datapath = '/home/pi/sensor-data';
 filename = 'data.csv';
 HEADERLINE = 'Time Temperature Humidity Dewpoint\n';
 dataline = '';
-/*
-//read sensor data
-async.series([
-    sht10.init,
-    sht10.reset,
-    function (callback) {
-        sht10.getSensorValues(function (error, values) {
-            dataline = dataline + Math.floor(Date.now() / 1000); //Timestamp from milliseconds to seconds.
-            dataline = dataline + '\t' + values.temperature + '\t' + values.humidity + '\t' + values.dewpoint + '\n';
-            writesensordata();
-            callback(error);
-        });
-    }
-], function (error) {
-    sht10.shutdown();
-    if (error) {
-        console.error(error);
-    }
-});
-*/
 
-var SHT1x = require('pi-sht1x');
+var async = require('async');
+
+var SHT1x = require('./js/SHT1x');
+
 
 async.series([
   SHT1x.init,
   SHT1x.reset,
   function(callback) {
     SHT1x.getSensorValues(function(error, values) {
+        console.log("ok");
       console.log(values);
       callback(error);
     });
   }
 ], function(error) {
   SHT1x.shutdown();
+       console.log("nok");
   if (error) {
     console.error(error);
   }
 });
-
+/*
 function writesensordata(callback) {
     //Write sensor data to given file
     fs.access(datapath + '/' + filename, fs.F_OK, function (err) {
@@ -64,3 +47,4 @@ function writesensordata(callback) {
         });
     });
 }
+*/
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import csv
 from pi_sht1x import SHT1x
 from os import system
 
@@ -13,15 +14,37 @@ SCK_PIN = 23;
 
 def main():
      with SHT1x(DATA_PIN, SCK_PIN, gpio_mode=GPIO.BCM) as sensor:
-          while 1:
-               system("clear");
-               temp = sensor.read_temperature()
-               humidity = sensor.read_humidity(temp)
-               sensor.calculate_dew_point(temp, humidity)
-               print(sensor)
-               time.sleep(2)
+          # field names  
+          fields = ['Temp', 'Humidity']  
+              
+        
+          # name of csv file  
+          filename = "measurements.csv"
+              
+          # writing to csv file  
+          with open(filename, 'a+') as csvfile:  
+               # creating a csv writer object  
+               csvwriter = csv.writer(csvfile, delimiter=';')  
+                  
+               # writing the fields  
+               csvwriter.writerow(fields)  
+                  
+          
+               while 1:
+                    system("clear");
+                    temp = sensor.read_temperature()
+                    humidity = sensor.read_humidity(temp)
+                    sensor.calculate_dew_point(temp, humidity)
+                    print(sensor)
+                    #print("{\"temperature\" : %5.2f}" % temp)
+                    
+                    with open(filename, 'a+') as csvfile:  
+                         # creating a csv writer object  
+                         csvwriter = csv.writer(csvfile, delimiter=';')  
+                         fields = [temp, humidity]  
+                         csvwriter.writerow(fields)                           
+                    time.sleep(2)
                
           
-
 
 main();
